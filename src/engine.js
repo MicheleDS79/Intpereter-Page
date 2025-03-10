a11257538dbe3b186830d0da499aa742fb0cf451
Mike Sherokee <151487504+MicheleDS79@users.noreply.github.com>
2025-03-09 21:19:42 -0400

    Fixed Sidebar Positioning, JS Working, Added Additional Link pages

--- src/engine.js
+++ src/engine.js
@@ -1,10 +1,10 @@
 function showSidebar() {
     const sidebar = document.querySelector('.main-sidebar')
-    sidebar.style.display ='flex'
+    sidebar.style.display = 'flex'
 }
 
 function hideSidebar() {
     const sidebar = document.querySelector('.main-sidebar')
-    sidebar.style.display = 'none';
+    sidebar.style.display = 'none'
 }
 /* open/close hamburger menu, will add effects later*/
