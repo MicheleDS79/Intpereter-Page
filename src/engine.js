9e0bd2c2f6227c8439157449734125768164c169
Mike Sherokee <151487504+MicheleDS79@users.noreply.github.com>
2025-03-09 22:56:03 -0400

    Added Additional Background images + Second styles page

--- src/engine.js
+++ src/engine.js
@@ -8,3 +8,28 @@ function hideSidebar() {
     sidebar.style.display = 'none'
 }
 /* open/close hamburger menu, will add effects later*/
+
+const buttons =
+document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click",
+() => {
+    const currentState = 
+button.getAttribute("data-state");
+
+    if (!currentState || 
+currentState === "closed") {
+    button.setAttribute("data-state", "opened");
+    button.setAttribute("aria-expanded", "expanded", "true");
+} else {
+    button.setAttribute("data-state", "closed");
+    button.setAttribute("aria-expanded", "false");
+        } 
+    });
+});
+
+        
+
+
+    
